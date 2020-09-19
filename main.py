fac943c5e98efe8570b7854a4acef33b2892d0f8
MASHOD0 <mashhood2002@gmail.com>
2020-09-20 00:58:55 +0530

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
-@app.route("/")
-def login():
-    return index.html
+@app.route('/')
+def ping():
+    return "Pong!!"
 
-
-if __name__ == "__main__":
-    app.run()
+app.run(port=2020)
